8222f0e50adc7e2110beb2dbbbc532c98518c301
bdm <bdm1999@yandex.ru>
2018-11-07 16:32:59 +0500

    add test for parsing check

--- src/test/scala/tinkoff/fintech/service/ServiceSpec.scala
+++ src/test/scala/tinkoff/fintech/service/ServiceSpec.scala
@@ -1,5 +1,10 @@
 package tinkoff.fintech.service
 
-class ServiceSpec extends FlatSpec with Matchers {
+import org.scalatest.{FlatSpec, Matchers}
 
+class ServiceSpec extends FlatSpec with Matchers {
+  "Parser" should "parse string" in {
+    val s = "milk, 90\n chicken, 150.4"
+    Parser.checkParse(s) shouldBe Seq(Price("milk", 90), Price("chicken", 150.4))
+  }
 }
