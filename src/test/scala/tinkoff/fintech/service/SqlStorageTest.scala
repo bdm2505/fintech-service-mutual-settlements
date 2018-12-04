a188befe19e59432397f7682e643aa6334305403
VikR <vs@vikr.su>
2018-12-04 14:21:48 +0500

    add tests for SqlStorage

---
+++ src/test/scala/tinkoff/fintech/service/SqlStorageTest.scala
@@ -0,0 +1,71 @@
+package tinkoff.fintech.service
+
+import java.sql.DriverManager
+
+import cats.effect.IO
+import com.dimafeng.testcontainers.{ForAllTestContainer, PostgreSQLContainer}
+import doobie.util.transactor.Transactor
+import doobie.util.transactor.Transactor.Aux
+import org.scalatest.{AsyncFlatSpec, Matchers}
+import tinkoff.fintech.service.data.{Check, Client, Product}
+import tinkoff.fintech.service.storage.SqlStorage
+
+
+class SqlStorageTest extends AsyncFlatSpec with Matchers with ForAllTestContainer {
+
+  override val container = PostgreSQLContainer()
+
+  lazy val storage: SqlStorage = new SqlStorage {
+    override val transactor: Aux[IO, Unit] = {
+      Transactor.fromDriverManager[IO](
+        container.driverClassName,
+        container.jdbcUrl,
+        container.username,
+        container.password
+      )
+    }
+  }
+
+  it should "container started" in {
+    Class.forName(container.driverClassName)
+    val connection = DriverManager.getConnection(container.jdbcUrl, container.username, container.password)
+    connection.createStatement().execute("SELECT 1") shouldEqual true
+  }
+
+  it should "create tables" in {
+    storage.transact {
+      for {
+        num <- storage.createTables
+      } yield num shouldBe 0
+    }
+  }
+
+  val client = Client(Some(1), "bob", "bob@y.ru", Some("9000"), None)
+  it should "save and find client" in {
+    storage.transact {
+      for {
+        id <- storage.saveNewClient(client)
+        foundClient <- storage.findClient(id)
+      } yield foundClient shouldBe client
+    }
+  }
+
+  val check = Check(Some(1), Seq(Product(Some(1), "milk", 90, None)), client)
+  it should "save ans find check" in {
+    storage.transact {
+      for {
+        id <- storage.saveNewCheck(check)
+        foundCheck <- storage.findCheck(id)
+      } yield foundCheck shouldBe check
+    }
+  }
+
+  it should "update check" in {
+    storage.transact {
+      for {
+        _ <- storage.updateCheck(check.connect(client, 1))
+        foundCheck <- storage.findCheck(check.id.get)
+      } yield foundCheck shouldBe check.connect(client, 1)
+    }
+  }
+}
